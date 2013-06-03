0509f80875e715c10a9f9d509e79df8dbde9bab8
z4c <zakil@free.fr>
2013-06-03 17:42:29 +0300

    Update managers.py

--- undelete/managers.py
+++ undelete/managers.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.db.models.query import QuerySet
 
 class TrashQuerySet(QuerySet):
-    
     def delete(self):
         for item in self.all():
             item.delete()
@@ -10,10 +9,10 @@ class TrashQuerySet(QuerySet):
 class NonTrashedManager(models.Manager):
     ''' Query only objects which have not been trashed. '''
     def get_query_set(self):
-        return TrashQuerySet(self.model, using=self._db).filter(trashed_at__isnull=True)
+        return TrashQuerySet(self.model, using=self._db).filter(deleted_at__isnull=True)
 
 class TrashedManager(models.Manager):
     ''' Query only objects which have been trashed. '''
     def get_query_set(self):
         query_set = super(TrashManager, self).get_query_set()
-        return query_set.filter(trashed_at__isnull=False)
+        return query_set.filter(deleted_at__isnull=False)
