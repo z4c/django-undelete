c5738026282f0fe8773bf0c7c954a41f97d9d3f9
z4c <zakil@free.fr>
2013-06-03 16:41:20 +0200

    Update models.py

--- undelete/models.py
+++ undelete/models.py
@@ -1,23 +1,22 @@
 from datetime import datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-
 from undelete.managers import TrashedManager, NonTrashedManager
 
-class TrashableMixin(models.Model):
-	trashed_at = models.DateTimeField(_('Trashed'), editable=False, blank=True, null=True)
+
+class Trashable(models.Model):
+	deleted_at = models.DateTimeField(_('Trashed'), editable=False,
+		blank=True, null=True, db_index=True, default=None)
 
 	objects = NonTrashedManager()
 	trash = TrashedManager()
 
-	def delete(self, *args, **kwargs, trash=True):
-	    if not self.trashed_at and trash:
-	        self.trashed_at = datetime.now()
+	def delete(self, *args, **kwargs, really=False):
+	    if not really:
+	        self.deleted_at = datetime.now()
 	        self.save()
 	    else:
-			super(SomeModel, self).delete(*args, **kwargs)
+		super(TrashableMixin, self).delete(*args, **kwargs)
 
-	def restore(self, commit=True):
-		self.trashed_at = None
-		if commit:
-			self.save()
+	def restore(self):
+		self.deleted_at = None
